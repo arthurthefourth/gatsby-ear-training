1e80e7b9f7979b4a689b200c35b94375ea36d7d5
Arthur Lewis <arthur.lewis@gmail.com>
2020-03-31 21:33:37 -0400

    Move synth up to NoteGrid component

--- src/components/note-block.js
+++ src/components/note-block.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react"
 import { Paper } from "@material-ui/core"
-import Synth from "./synth"
 
 class NoteBlock extends Component {
-  handleClick() {
-    const synth = new Synth()
-    synth.playNote("C3")
+  constructor(props) {
+    super(props)
+    this.state = { color: "grey" }
+  }
+
+  handleClick = (event) => {
+    this.props.synth.playNote("C3")
+    this.setState({ color: "blue" })
   }
 
   render() {
@@ -13,7 +17,11 @@ class NoteBlock extends Component {
       <Paper
         variant="outlined"
         elevation={10}
-        style={{ width: 100, height: 100, backgroundColor: "grey" }}
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: `${this.state.color}`,
+        }}
         onClick={this.handleClick}
       />
     )
--- src/components/note-grid.js
+++ src/components/note-grid.js
@@ -1,45 +1,56 @@
-import React, { Fragment } from "react"
+import React, { Component } from "react"
 import { Grid } from "@material-ui/core"
 import NoteBlock from "./note-block"
+import Synth from "./synth"
+
 import { Hearing, Straighten } from "@material-ui/icons"
 
-const NoteGrid = () => (
-  <div style={{ marginTop: 100, marginBottom: 100 }}>
-    <Grid container justify="center" spacing={2}>
-      <Grid item style={{ alignSelf: "center" }}>
-        <Hearing fontSize="large" />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-    </Grid>
-    <Grid container justify="center" spacing={2}>
-      <Grid item style={{ alignSelf: "center" }}>
-        <Straighten fontSize="large" />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-      <Grid item>
-        <NoteBlock />
-      </Grid>
-    </Grid>
-  </div>
-)
+class NoteGrid extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { synth: new Synth() }
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: 100, marginBottom: 100 }}>
+        <Grid container justify="center" spacing={2}>
+          <Grid item style={{ alignSelf: "center" }}>
+            <Hearing fontSize="large" />
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+        </Grid>
+        <Grid container justify="center" spacing={2}>
+          <Grid item style={{ alignSelf: "center" }}>
+            <Straighten fontSize="large" />
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+          <Grid item>
+            <NoteBlock synth={this.state.synth}/>
+          </Grid>
+        </Grid>
+      </div>
+    )
+  }
+}
 
 export default NoteGrid
