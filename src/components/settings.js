0c80e786157f2fefafe0b98fe0de176666f5e6ce
Arthur Lewis <arthur.lewis@gmail.com>
2020-04-19 11:18:58 -0400

    Move Settings into their own component

---
+++ src/components/settings.js
@@ -0,0 +1,113 @@
+import React from "react"
+import { 
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  Grid,
+  Typography,
+} from "@material-ui/core"
+
+import Fader from "./fader"
+import LearnButton from "./learn-button"
+
+import { ExpandMore } from "@material-ui/icons"
+
+const keys = [
+  {
+    value: 0,
+    label: "C",
+  },
+  {
+    value: 1,
+    label: "Db",
+  },
+  {
+    value: 2,
+    label: "D",
+  },
+  {
+    value: 3,
+    label: "Eb",
+  },
+  {
+    value: 4,
+    label: "E",
+  },
+  {
+    value: 5,
+    label: "F",
+  },
+  {
+    value: 6,
+    label: "Gb",
+  },
+  {
+    value: 7,
+    label: "G",
+  },
+  {
+    value: 8,
+    label: "Ab",
+  },
+  {
+    value: 9,
+    label: "A",
+  },
+  {
+    value: 10,
+    label: "Bb",
+  },
+  {
+    value: 11,
+    label: "B",
+  },
+]
+
+const Settings = () => {
+  return (
+    <ExpansionPanel>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMore />}
+        aria-controls="settings-content"
+        id="settings-header"
+      >
+        <Typography>Settings</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Grid container spacing={10}>
+          <Grid item xs={12} md={4}>
+            <Fader label="Key" min={0} max={11} marks={keys} />
+            <LearnButton />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Fader
+              label="Tempo"
+              min={60}
+              max={240}
+              defaultValue={120}
+              step={10}
+              valueLabelDisplay="on"
+              marks={[60, 240].map(number => {
+                return { value: number, label: number }
+              })}
+            />{" "}
+            <LearnButton />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Fader
+              label="Phrase Length"
+              min={2}
+              max={8}
+              marks={[2, 3, 4, 5, 6, 7, 8].map(number => {
+                return { value: number, label: number }
+              })}
+            />
+            <LearnButton />
+          </Grid>
+        </Grid>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  )
+}
+
+export default Settings
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,9 @@
 import React, { Component } from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import Fader from "../components/fader"
-import LearnButton from "../components/learn-button"
-import NoteGrid from "../components/note-grid"
-import StartButton from "../components/start-button"
 import Game from "../components/game"
-
-import {
-  Container,
-  Grid,
-  Slider,
-  Typography,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
-} from "@material-ui/core"
-
-import { ExpandMore } from "@material-ui/icons"
-
-const keys = [
-  {
-    value: 0,
-    label: "C",
-  },
-  {
-    value: 1,
-    label: "Db",
-  },
-  {
-    value: 2,
-    label: "D",
-  },
-  {
-    value: 3,
-    label: "Eb",
-  },
-  {
-    value: 4,
-    label: "E",
-  },
-  {
-    value: 5,
-    label: "F",
-  },
-  {
-    value: 6,
-    label: "Gb",
-  },
-  {
-    value: 7,
-    label: "G",
-  },
-  {
-    value: 8,
-    label: "Ab",
-  },
-  {
-    value: 9,
-    label: "A",
-  },
-  {
-    value: 10,
-    label: "Bb",
-  },
-  {
-    value: 11,
-    label: "B",
-  },
-]
+import Settings from "../components/settings"
 
 class IndexPage extends Component {
   render() {
@@ -78,49 +11,7 @@ class IndexPage extends Component {
       <Layout>
         <SEO title="Home" />
         <Game />
-        {/* Settings */}
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="settings-content"
-            id="settings-header"
-          >
-            <Typography>Settings</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Grid container spacing={10}>
-              <Grid item xs={12} md={4}>
-                <Fader label="Key" min={0} max={11} marks={keys} />
-                <LearnButton />
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Fader
-                  label="Tempo"
-                  min={60}
-                  max={240}
-                  defaultValue={120}
-                  step={10}
-                  valueLabelDisplay="on"
-                  marks={[60, 240].map(number => {
-                    return { value: number, label: number }
-                  })}
-                />{" "}
-                <LearnButton />
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Fader
-                  label="Phrase Length"
-                  min={2}
-                  max={8}
-                  marks={[2, 3, 4, 5, 6, 7, 8].map(number => {
-                    return { value: number, label: number }
-                  })}
-                />
-                <LearnButton />
-              </Grid>
-            </Grid>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
+        <Settings />
       </Layout>
     )
   }
