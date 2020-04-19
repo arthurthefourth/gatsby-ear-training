d0166aa5b031504e47ea102b4c53e0bfaa8363f1
Arthur Lewis <arthur.lewis@gmail.com>
2020-04-18 22:57:41 -0400

    Use major scale as available pitches for sequence

--- src/components/sequence-generator.js
+++ src/components/sequence-generator.js
@@ -1,17 +1,44 @@
 import { sample } from "lodash"
 
+function numberToNoteName(number) {
+  const pitchClasses = [
+    "C",
+    "Db",
+    "D",
+    "Eb",
+    "E",
+    "F",
+    "Gb",
+    "G",
+    "Ab",
+    "A",
+    "Bb",
+    "B",
+  ]
+
+  const pitch = pitchClasses[number % 12]
+  const octave = Math.floor(number / 12) - 1
+
+  return `${pitch}${octave}`
+}
+
+const majorScale = [0, 2, 4, 5, 7, 9, 11, 12]
+const majorTriad = [0, 4, 7]
+const root = 48 // Middle C
+const availablePitches = majorScale.map(interval => {
+  return numberToNoteName(root + interval)
+})
+
 class SequenceGenerator {
   constructor(length) {
     this.length = length
   }
 
-  static availablePitches = ["C3", "E3", "G3"] // Major triad
-
   generate() {
     let i
     const sequence = []
     for (i = 0; i < this.length; i++) {
-      sequence.push(sample(SequenceGenerator.availablePitches))
+      sequence.push(sample(availablePitches))
     }
     return sequence
   }
